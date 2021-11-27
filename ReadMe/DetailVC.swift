29450b8133d15f15fbc44a8abfdaf065ef61fa64
vinh <dr.vinhhoang@gmail.com>
2021-11-27 13:39:51 +0700

    Add Image picker

--- ReadMe/DetailVC.swift
+++ ReadMe/DetailVC.swift
@@ -15,6 +15,17 @@ class DetailVC: UIViewController {
     @IBOutlet var authorLabel: UILabel!
     @IBOutlet var imageView: UIImageView!
     
+    
+    @IBAction func updateImage() {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerController.isSourceTypeAvailable(.camera) ? .camera : .photoLibrary
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = book.image
@@ -31,6 +42,20 @@ class DetailVC: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
+extension DetailVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.editedImage] as? UIImage else { return }
+        
+        imageView.image = selectedImage
+        Library.saveImage(selectedImage, forBook: book)
+        dismiss(animated: true)
+        
     }
     
 }
--- ReadMe/LibraryVC.swift
+++ ReadMe/LibraryVC.swift
@@ -23,6 +23,11 @@ class LibraryVC: UITableViewController {
     
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
     
     
     
