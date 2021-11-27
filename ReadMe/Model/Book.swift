365203bea3f5110f9aeb206249d98fa3b8727fa7
vinh <dr.vinhhoang@gmail.com>
2021-11-27 09:48:17 +0700

    initial commit

---
+++ ReadMe/Model/Book.swift
@@ -0,0 +1,17 @@
+//
+//  Book.swift
+//  ReadMe
+//
+//  Created by VINH HOANG on 27/11/2021.
+//
+
+import Foundation
+import UIKit
+
+struct Book {
+    let title: String
+    let author: String
+    var image: UIImage  {
+        LibrarySymbol.letterSquare(letter: title.first).image
+    }
+}
--- ReadMe/ViewController.swift
+++ ReadMe/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LibraryVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
+    }
+    
+    
+    
+    
+    // MARK: - DataSource
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Library.books.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath)
+        
+        let book = Library.books[indexPath.row]
+        
+        cell.textLabel?.text = book.title
+        cell.imageView?.image = book.image
+        
+        return cell
+    }
 
 }
 
